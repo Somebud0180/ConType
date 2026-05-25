f2e4fe056171d380aeac38081eb918beaad654c7
Somebud0180 <ethanlagera@icloud.com>
2026-05-25 13:11:54 +0800

    Added documentation to KeyEmitter

--- ConType/KeyEmitter.swift
+++ ConType/KeyEmitter.swift
@@ -7,12 +7,23 @@
 
 import ApplicationServices
 
+/// Handles posting CGEvents for keyboard emulation
 final class KeyEmitter {
+    /// Wraps `emit()` that accepts a `CGKeyCode` and parses `VirtualKey`'s  key code.
+    /// - Parameters:
+    ///   - key: The `VirtualKey` to be emitted
+    ///   - modifiers: the `CGEventFlags` to apply
+    /// - Returns: `true` if emitted successfuly, else `false`
     @discardableResult
     func emit(_ key: VirtualKey, modifiers: CGEventFlags = []) -> Bool {
         emit(keyCode: key.keyCode, modifiers: modifiers)
     }
-
+    
+    /// Posts a CGEvent of the key down and key up of the given key code and modifier.
+    /// - Parameters:
+    ///   - keyCode: The `CGKeyCode` to be emitted
+    ///   - modifiers: The `CGEventFlags` to be passed
+    /// - Returns: `true` if emitted successfuly, else `false`
     @discardableResult
     func emit(keyCode: CGKeyCode, modifiers: CGEventFlags = []) -> Bool {
         guard InputMonitoringPermission.isAuthorized() else { return false }
